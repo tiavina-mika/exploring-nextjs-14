39bb18b6f7592e10044a14225fbd0f20665a2ec0
tiavina-mika <tiavinamika@gmail.com>
2023-12-21 09:36:51 +0300

    add and use fonts

--- nextjs-dashboard/app/layout.tsx
+++ nextjs-dashboard/app/layout.tsx
@@ -1,11 +1,17 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+import { primaryFont } from '@/app/ui/fonts';
+import { ReactNode } from 'react';
+
+type Props = {
+  children: ReactNode
+}
+const RootLayout = ({ children }: Props) => {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${primaryFont} antialiased`}>
+        {children}
+      </body>
     </html>
   );
 }
+
+export default RootLayout
--- nextjs-dashboard/app/page.tsx
+++ nextjs-dashboard/app/page.tsx
@@ -1,12 +1,14 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
+import { secondaryFont } from './ui/fonts';
 
-export default function Page() {
+const Page = () => {
   return (
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
         {/* <AcmeLogo /> */}
+        <h1 className={secondaryFont}>Mik.</h1>
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
@@ -31,3 +33,5 @@ export default function Page() {
     </main>
   );
 }
+
+export default Page
