5cbaa0cd194e2bdda1f227fe44d47425128aa4f9
Tiavina Michael Ralainirina <tiavinamika@gmail.com>
2023-12-29 13:56:26 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -58,6 +58,7 @@
 // root entry should be here
 // root entry should be here
 // root entry should be here
+// root entry should be here
 r// root entry should be here
 // root entry should be here
 // root entry should be here
