381bd0d0be37b21fe8a095a75bf91215ae60c73f
tiavina-mika <tiavinamika@gmail.com>
2023-12-29 11:24:35 +0300

    remove unused files

--- nextjs-dashboard/app/layout.tsx
+++ nextjs-dashboard/app/layout.tsx
@@ -1,10 +1,7 @@
 import { primaryFont } from '@/app/ui/fonts';
 import { ReactNode } from 'react';
 import '@/app/ui/global.css';
-// import ThemeRegistry from '../components/ThemeRegistry';
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter'; // or `v14-appRouter` if you are using Next.js v14
-// import { theme } from '@/utils/theme';
-// import { ThemeProvider } from '@mui/material';
 import ThemeRegistry from '@/components/ThemeRegistry';
 
 type Props = {
@@ -19,7 +16,6 @@ const RootLayout = ({ children }: Props) => {
           {children}
         </ThemeRegistry>
       </AppRouterCacheProvider>
-        {/* <ThemeRegistry options={{ key: 'mui' }}>{children}</ThemeRegistry> */}
       </body>
     </html>
   );
