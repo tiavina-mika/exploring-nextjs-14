e3bbbdb2b29ec9801fb11833d5fe14c1a2be4b6d
tiavina-mika <tiavinamika@gmail.com>
2024-01-05 16:02:33 +0300

    use variant lib

--- nextjs-dashboard/app/page.tsx
+++ nextjs-dashboard/app/page.tsx
@@ -6,7 +6,7 @@ import Table from '@/components/table/Table';
 const Page = () => {
   return (
     <main className="flex min-h-screen flex-col p-6">
-      {/* <div className="flex bg-yellow-300 h-20 shrink-0 items-end rounded-lg p-4 md:h-52 gap-4">
+      <div className="flex bg-yellow-300 h-20 shrink-0 items-end rounded-lg p-4 md:h-52 gap-4">
         <Button>Primary</Button>
         <Button color="secondary">Secondary</Button>
         <Button color="error">Error</Button>
@@ -23,7 +23,7 @@ const Page = () => {
         <Button color="info" variant="outlined">Info</Button>
         <Button color="warning" variant="outlined">Warning</Button>
         <Button color="default" variant="outlined">default</Button>
-      </div> */}
+      </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           <Link
