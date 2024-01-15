b2bb5d2e6348b9c786a544b503619e61e07d64f1
tiavina-mika <tiavinamika@gmail.com>
2024-01-15 17:22:55 +0300

    config prettier

---
+++ nextjs-dashboard/.prettierrc.js
@@ -0,0 +1,42 @@
+
+/**
+ * Remember to restart VSCode after making
+ * any changes here and saving this file.
+ */
+module.exports = {
+  arrowParens: "always",
+  quoteProps: "preserve",
+  bracketSameLine: false,
+  endOfLine: "lf",
+  importOrder: [
+    "^~/styles/(.*)$",
+    "",
+    "^(react/(.*)$)|^(react$)",
+    "^(next/(.*)$)|^(next$)",
+    "<THIRD_PARTY_MODULES>",
+    "",
+    "^@/components/(.*)$",
+    "^@/config/(.*)$",
+    "^@/containers/(.*)$",
+    "^@/hooks/(.*)$",
+    "^@/validations/(.*)$",
+    "^@/utils/(.*)$",
+    "^@/env/(.*)$",
+    "",
+    "^[./]",
+    "",
+  ],
+  importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
+  plugins: [
+    "prettier-plugin-tailwindcss" // MUST come last
+  ],
+  printWidth: 80,
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "all",
+  useTabs: false,
+  importOrderSeparation: true,
+  importOrderCaseInsensitive: true,
+  importOrderGroupNamespaceSpecifiers: true
+};
