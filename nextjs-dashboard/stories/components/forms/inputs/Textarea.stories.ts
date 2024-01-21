5ea3cf338dc7ba2a6318a89311cfd974e214a963
tiavina-mika <tiavinamika@gmail.com>
2024-01-21 17:22:27 +0300

    refactor textarea

--- nextjs-dashboard/stories/components/forms/inputs/Textarea.stories.ts
+++ nextjs-dashboard/stories/components/forms/inputs/Textarea.stories.ts
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
-import { Textarea as TextareaComponent } from '@/components/forms/inputs/Textarea';
+import { Textarea as TextareaComponent } from '@/components/ui/Textarea';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
