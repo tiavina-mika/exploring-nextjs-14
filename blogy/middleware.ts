bd28259d4bc3c7143c667b3e9849d272d12f83f9
tiavina-mika <tiavinamika@gmail.com>
2024-02-27 10:20:28 +0300

    clean middleware

--- blogy/middleware.ts
+++ blogy/middleware.ts
@@ -4,7 +4,7 @@ import NextAuth from 'next-auth';
 import { translatedPathnames } from './config/routes';
 import { authConfig } from './config/auth.config';
 
-export const authMiddleware = NextAuth(authConfig).auth;
+// export const authMiddleware = NextAuth(authConfig).auth;
 
 export default createMiddleware({
   // A list of all locales that are supported
