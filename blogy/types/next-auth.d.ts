e55b304b22d4a8679aafdff6ab827954486d5665
tiavina-mika <tiavinamika@gmail.com>
2024-02-27 10:19:47 +0300

    udpate type

--- blogy/types/next-auth.d.ts
+++ blogy/types/next-auth.d.ts
@@ -1,11 +1,18 @@
-import NextAuth from "next-auth"
+import NextAuth, { DefaultSession } from "next-auth"
 import { IUser } from "./user.type"
 
 declare module "next-auth" {
   /**
    * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
    */
-  interface Session {
-    user: IUser
+  interface Session extends DefaultSession {
+    // maybe store another field later
+    /**
+     * name: string;
+     * email: string;
+     * id: string;
+     */
+    user: {} & DefaultSession["user"],
+    token: string;
   }
 }
