6a785690d1e1d49899ed8c946a1482629909d5be
tiavina-mika <tiavinamika@gmail.com>
2024-02-28 16:41:35 +0300

    using multiple middleware

--- blogy/middleware.ts
+++ blogy/middleware.ts
@@ -1,12 +1,10 @@
 import { locales, defaultLocale, localePrefix } from '@/config/i18n';
 import createMiddleware from 'next-intl/middleware';
-import NextAuth from 'next-auth';
 import { translatedPathnames } from './config/routes';
-import { authConfig } from './config/auth.config';
 
-// export const authMiddleware = NextAuth(authConfig).auth;
+import { auth } from './config/auth.config';
 
-export default createMiddleware({
+export default auth(createMiddleware({
   // A list of all locales that are supported
   locales,
   // Used when no locale matches
@@ -15,7 +13,7 @@ export default createMiddleware({
   // add prefix even with default locale
   localePrefix,
   pathnames: translatedPathnames
-});
+}));
  
 export const config = {
   // Match only internationalized pathnames
