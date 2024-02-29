c61fa845d300b5fc26a47a0948ad25421d54c84a
tiavina-mika <tiavinamika@gmail.com>
2024-02-29 13:09:38 +0300

    Update navigation.ts

--- blogy/config/navigation.ts
+++ blogy/config/navigation.ts
@@ -1,11 +1,11 @@
 import { createLocalizedPathnamesNavigation } from 'next-intl/navigation';
 
 import { localePrefix, locales } from './i18n';
-import { translatedPathnames } from './routes';
+import { translatedRoutes } from './routes';
 
 export const { Link, redirect, usePathname, useRouter, getPathname } =
   createLocalizedPathnamesNavigation({
     locales,
     localePrefix,
-    pathnames: translatedPathnames,
+    pathnames: translatedRoutes,
   });
