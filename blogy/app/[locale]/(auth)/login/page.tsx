3d4ffc5558186faa0a93fdf6807ba6bd58d78fee
tiavina-mika <tiavinamika@gmail.com>
2024-03-02 13:26:09 +0300

    Update page.tsx

--- blogy/app/[locale]/(auth)/login/page.tsx
+++ blogy/app/[locale]/(auth)/login/page.tsx
@@ -3,6 +3,9 @@ import { unstable_setRequestLocale } from 'next-intl/server';
 import Title from '@/components/typography/Title';
 import { Locale } from '@/config/i18n';
 import LoginFormProvider from '@/containers/auth/login/LoginFormProvider';
+import { ROUTES } from '@/config/routes';
+import AuthLink from '@/containers/auth/AuthLink';
+import { useTranslations } from 'next-intl';
 
 type Props = {
   params: {
@@ -12,13 +15,16 @@ type Props = {
 
 const LoginPage = ({ params: { locale } }: Props) => {
   unstable_setRequestLocale(locale);
+  const t = useTranslations('User');
 
-  
   return (
     <main className="flex items-center justify-center md:h-screen">
       <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
-        <Title>Login</Title>
+        <Title>{t('login')}</Title>
         <LoginFormProvider />
+        <div>
+          <AuthLink text={t('login')} url={ROUTES.signUp} label={t('haveNoAccountYet')} />
+        </div>
       </div>
     </main>
   );
