e61ed815ce0e346d62d8ac3238a5c7f17c99c220
tiavina-mika <tiavinamika@gmail.com>
2024-03-09 11:23:00 +0300

    Update site.ts

--- blogy/config/site.ts
+++ blogy/config/site.ts
@@ -4,8 +4,6 @@ import { APP_NAME, AUTHOR_USERNAME } from '@/utils/constants';
 
 export const siteConfig = {
   name: APP_NAME,
-  description:
-    'Blogy is a simple web application using NextJs, it serves as a boilerplate for NextJs 14 applications',
   url: env.NEXT_PUBLIC_APP_URL,
   links: { github: 'https://github.com/tiavina-mika' },
   author: AUTHOR_USERNAME,
