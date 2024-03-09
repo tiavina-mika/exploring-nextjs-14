ff00d5f0e8493cb01989998bac8ac53819872a94
tiavina-mika <tiavinamika@gmail.com>
2024-03-09 12:06:31 +0300

    Update NavBarItem.tsx

--- blogy/components/navBar/NavBarItem.tsx
+++ blogy/components/navBar/NavBarItem.tsx
@@ -8,7 +8,7 @@ import { IMenu } from '@/types/app.type';
 import TextLink, { TextLinkProps } from '../typography/TextLink';
 import NextIcon from '../NextIcon';
 
-const isLinkActive = (currentPath: string, href: IMenu['value']) => {
+const isLinkActive = (currentPath: string, href: IMenu['value']): boolean => {
   if ((href as any).pathname) {
     return currentPath.startsWith((href as any).pathname);
   }
