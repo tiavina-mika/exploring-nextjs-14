cb565abb00e45370d691d91236f7d461e7ebcd9f
tiavina-mika <tiavinamika@gmail.com>
2024-03-09 11:59:13 +0300

    Update page.tsx

--- blogy/app/[locale]/dashboard/page.tsx
+++ blogy/app/[locale]/dashboard/page.tsx
@@ -1,8 +1,31 @@
-import { unstable_setRequestLocale } from 'next-intl/server';
+import { getTranslations, unstable_setRequestLocale } from 'next-intl/server';
 
 import Title from '@/components/typography/Title';
 import { Locale } from '@/config/i18n';
+import { Metadata } from 'next';
 
+// ----------------------------- //
+// -------- metadata ----------- //
+// ----------------------------- //
+type MetaDataProps = {
+  params: { locale: Locale }
+}
+
+export const generateMetadata = async ({ params: { locale }}: MetaDataProps): Promise<Metadata> => {
+  const t = await getTranslations({
+    locale,
+    namespace: 'Metadata',
+  });
+
+  return {
+    title: t('dashboard.metaTitle'),
+    description: t('dashboard.metaDescription'),
+  };
+}
+
+// ----------------------------- //
+// ------------ page ----------- //
+// ----------------------------- //
 type Props = {
   params: {
     locale: Locale;
