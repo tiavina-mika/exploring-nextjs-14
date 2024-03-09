71a86efcc7bbb451ecdadc1ef69ec1c8cbee4463
tiavina-mika <tiavinamika@gmail.com>
2024-03-09 11:21:39 +0300

    Update page.tsx

--- blogy/app/[locale]/(auth)/login/page.tsx
+++ blogy/app/[locale]/(auth)/login/page.tsx
@@ -1,4 +1,4 @@
-import { unstable_setRequestLocale } from 'next-intl/server';
+import { getTranslations, unstable_setRequestLocale } from 'next-intl/server';
 
 import Title from '@/components/typography/Title';
 import { Locale } from '@/config/i18n';
@@ -7,6 +7,23 @@ import { ROUTES } from '@/config/routes';
 import AuthLink from '@/containers/auth/AuthLink';
 import { useTranslations } from 'next-intl';
 import AuthWithProviders from '@/containers/auth/AuthWithProviders';
+import { Metadata } from 'next';
+
+type MetaDataProps = {
+  params: { locale: Locale }
+}
+
+export const generateMetadata = async ({ params: { locale }}: MetaDataProps): Promise<Metadata> => {
+  const t = await getTranslations({
+    locale,
+    namespace: 'Metadata',
+  });
+
+  return {
+    title: t('login.metaTitle'),
+    description: t('login.metaDescription'),
+  };
+}
 
 type Props = {
   params: {
