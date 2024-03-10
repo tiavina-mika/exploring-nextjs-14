464ed3faf13f394a1be99816f51c03a98193b6fd
tiavina-mika <tiavinamika@gmail.com>
2024-03-10 13:34:43 +0300

    Update app.utils.ts

--- blogy/utils/app.utils.ts
+++ blogy/utils/app.utils.ts
@@ -1,4 +1,5 @@
 import env from '@/env';
+import { IPaginationQuery } from '@/types/app.type';
 import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
@@ -6,3 +7,5 @@ export const cn = (...inputs: ClassValue[]) => twMerge(clsx(inputs));
 
 export const getAbsoluteUrl = (path?: string): string =>
   `${env.NEXT_PUBLIC_APP_URL}${path}`;
+
+export const getPaginatedQuery = (perPage: number, page: number): IPaginationQuery => ({ limit: perPage, skip: (page - 1) * perPage})
