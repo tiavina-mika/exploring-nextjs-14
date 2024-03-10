7c4944e79ce77205803f8c109b85de1a6e174270
tiavina-mika <tiavinamika@gmail.com>
2024-03-10 15:29:55 +0300

    Update page.tsx

--- blogy/app/[locale]/dashboard/articles/page.tsx
+++ blogy/app/[locale]/dashboard/articles/page.tsx
@@ -3,7 +3,6 @@ import { QueryClient } from '@tanstack/query-core';
 import { getTranslations, unstable_setRequestLocale } from 'next-intl/server';
 
 import Text from '@/components/typography/Text';
-import Title from '@/components/typography/Title';
 import { Locale } from '@/config/i18n';
 import Articles from '@/containers/articles/Articles';
 import ReactQueryServerHydration from '@/providers/ReactQueryServerHydration';
@@ -12,6 +11,8 @@ import Pagination from '@/components/Pagination';
 import { getPaginatedQuery } from '@/utils/app.utils';
 import { Breadcrumbs } from '@/components/Breadcrumbs';
 import { ROUTES } from '@/config/routes';
+import { PAGINATION } from '@/utils/constants';
+import { paginationSearchParamsSchema } from '@/validations/app.validations';
 
 // ----------------------------- //
 // -------- metadata ----------- //
@@ -35,6 +36,8 @@ export const generateMetadata = async ({ params: { locale }}: MetaDataProps): Pr
 // ----------------------------- //
 // ------------ page ----------- //
 // ----------------------------- //
+const perPage = PAGINATION.perPage;
+
 type Props = {
   params: {
     locale: Locale;
@@ -46,8 +49,9 @@ type Props = {
 
 const ArticlesPage = async ({ params: { locale }, searchParams }: Props) => {
   unstable_setRequestLocale(locale);
-  const perPage = 1;
-  const page = parseInt(searchParams.page, 10) || 1;
+  const parseSearchParams = paginationSearchParamsSchema.parse(searchParams);
+  console.log('parseSearchParams: ', parseSearchParams);
+  const page = parseInt(parseSearchParams.page, 10) || 1;
 
   const t = await getTranslations('Article')
 
