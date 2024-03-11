ff5a01fb2ada3a95fc9f52cdf0b0f6635a42838f
tiavina-mika <tiavinamika@gmail.com>
2024-03-11 10:32:49 +0300

    Create page.tsx

---
+++ blogy/app/[locale]/(auth)/verify-account/page.tsx
@@ -0,0 +1,67 @@
+import { getTranslations, unstable_setRequestLocale } from 'next-intl/server';
+
+import Title from '@/components/typography/Title';
+import { Locale } from '@/config/i18n';
+import AuthLink from '@/containers/auth/AuthLink';
+import { ROUTES } from '@/config/routes';
+import { useTranslations } from 'next-intl';
+import { Metadata } from 'next';
+import VerifyAccountFormProvider from '@/containers/auth/verifyAccount/VerifyAccountFormProvider';
+import Text from '@/components/typography/Text';
+
+// ----------------------------- //
+// -------- metadata ----------- //
+// ----------------------------- //
+type MetaDataProps = {
+  params: { locale: Locale }
+}
+
+export const generateMetadata = async ({ params: { locale }}: MetaDataProps): Promise<Metadata> => {
+  const t = await getTranslations({
+    locale,
+    namespace: 'Metadata',
+  });
+
+  return {
+    title: t('verifyAccount.metaTitle'),
+    description: t('verifyAccount.metaDescription'),
+  };
+}
+
+// ----------------------------- //
+// ------------ page ----------- //
+// ----------------------------- //
+type Props = {
+  params: {
+    locale: Locale;
+  };
+};
+
+const VerifyAccountPage = ({ params: { locale } }: Props) => {
+  unstable_setRequestLocale(locale);
+  const t = useTranslations('User');
+  const tAuth = useTranslations('Auth');
+
+  return (
+    <div className='flex flex-col items-center'>
+      <div  className='flex flex-col self-stretch space-y-2 items-center'>
+        <div className="flex flex-col items-center mb-8 gap-3">
+          <Title level="h2" className="text-2xl">
+            {tAuth('emailVerification')}
+          </Title>
+          <Text>
+            {tAuth('enterCodeReceivedByEmail')}
+          </Text>
+        </div>
+        <div className="self-stretch flex justify-center">
+          <VerifyAccountFormProvider />
+        </div>
+        <div className="mt-4">
+          <AuthLink text={t('login')} url={ROUTES.login} label={t('alreadyHaveAccount')} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default VerifyAccountPage;
