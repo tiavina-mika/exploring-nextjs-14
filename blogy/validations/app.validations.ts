2463a2eacc26a0c2622c17698d19ba0b7b4bd506
tiavina-mika <tiavinamika@gmail.com>
2024-03-11 09:08:04 +0300

    Update app.validations.ts

--- blogy/validations/app.validations.ts
+++ blogy/validations/app.validations.ts
@@ -2,4 +2,6 @@ import { z } from "zod";
 
 export const paginationSearchParamsSchema = z.object({
   page: z.string().optional().default("1"),
+  field: z.string().optional(),
+  order: z.union([z.literal('asc'), z.literal('desc')]).optional().default("asc"),
 });
