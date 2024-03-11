8d66579b509bbbd9355b0a9a0a0d9af6eade7054
tiavina-mika <tiavinamika@gmail.com>
2024-03-11 15:38:14 +0300

    Create page.tsx

---
+++ blogy/app/[locale]/(auth)/forgot-password/page.tsx
@@ -0,0 +1,71 @@
+import { getTranslations, unstable_setRequestLocale } from 'next-intl/server';
+
+import Title from '@/components/typography/Title';
+import { Locale } from '@/config/i18n';
+import AuthLink from '@/containers/auth/AuthLink';
+import { ROUTES } from '@/config/routes';
+import { useTranslations } from 'next-intl';
+import { Metadata } from 'next';
+import VerifyAccountFormProvider from '@/containers/auth/verifyAccount/VerifyAccountFormProvider';
+import Text from '@/components/typography/Text';
+import Button from '@/components/buttons/Button';
+import EmailResetPasswordFormProvider from '@/containers/auth/email/EmailResetPasswordFormProvider';
+import { uncapitalize } from 'string-ts';
+
+// ----------------------------- //
+// -------- metadata ----------- //
+// ----------------------------- //
+type MetaDataProps = {
+  params: { locale: Locale }
+}
+
+export const generateMetadata = async ({ params: { locale }}: MetaDataProps): Promise<Metadata> => {
+  const t = await getTranslations({
+    locale,
+    namespace: 'Metadata',
+  });
+
+  return {
+    title: t('forgotPassword.metaTitle'),
+    description: t('forgotPassword.metaDescription'),
+  };
+}
+
+// ----------------------------- //
+// ------------ page ----------- //
+// ----------------------------- //
+type Props = {
+  params: {
+    locale: Locale;
+  };
+};
+
+const ForgotPasswordPage = ({ params: { locale } }: Props) => {
+  unstable_setRequestLocale(locale);
+  const tAuth = useTranslations('Auth');
+
+  return (
+    <div className='flex flex-col items-center'>
+      <div  className='flex flex-col self-stretch space-y-2 items-center'>
+        {/* titles */}
+        <div className="flex flex-col items-center mb-8 gap-3">
+          <Title level="h2" className="text-2xl text-center">
+            {tAuth('passwordResetRequest')}
+          </Title>
+        </div>
+
+        {/* form */}
+        <div className="self-stretch">
+          <EmailResetPasswordFormProvider />
+        </div>
+
+        {/* link to login page */}
+        <div className="!mt-8">
+          <AuthLink text={uncapitalize(tAuth('connection'))} url={ROUTES.login} label={tAuth('returnTo')} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ForgotPasswordPage;
