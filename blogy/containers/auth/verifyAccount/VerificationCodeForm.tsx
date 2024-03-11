c122cdcad3560b4bfc44b241cdef2c3de8139dd2
tiavina-mika <tiavinamika@gmail.com>
2024-03-11 11:50:00 +0300

    Create VerificationCodeForm.tsx

---
+++ blogy/containers/auth/verifyAccount/VerificationCodeForm.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import CodeVerification from '@/components/forms/inputs/CodeVerification';
+
+const VerificationCodeForm = () => {
+  const handleCodeVerification = (code: string) => {
+    console.log('code', code);
+  }
+
+
+  return (
+    <CodeVerification onComplete={handleCodeVerification} />
+  );
+};
+
+export default VerificationCodeForm;
