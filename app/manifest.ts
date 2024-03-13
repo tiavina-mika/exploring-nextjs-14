d182deb42800295d1aac4c048ee6a42acfa4db62
tiavina-mika <tiavinamika@gmail.com>
2024-03-13 17:36:40 +0300

    Create manifest.ts

---
+++ app/manifest.ts
@@ -0,0 +1,22 @@
+import { MetadataRoute } from 'next'
+ 
+const manifest = (): MetadataRoute.Manifest => {
+  return {
+    name: 'Next.js App',
+    short_name: 'Next.js App',
+    description: 'Next.js App',
+    start_url: '/',
+    display: 'standalone',
+    background_color: '#fff',
+    theme_color: '#fff',
+    icons: [
+      {
+        src: '/favicon.ico',
+        sizes: 'any',
+        type: 'image/x-icon',
+      },
+    ],
+  }
+}
+
+export default manifest
