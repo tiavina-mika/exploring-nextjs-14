bca03fec9bec115d0c98fc6a64a15c76ea3a7a5a
tiavina-mika <tiavinamika@gmail.com>
2024-03-13 18:53:18 +0300

    Create site.ts

---
+++ config/site.ts
@@ -0,0 +1,13 @@
+import env from '@/env';
+
+import { APP_NAME, AUTHOR_USERNAME } from '@/utils/constants';
+
+export const siteConfig = {
+  name: APP_NAME,
+  url: env.NEXT_PUBLIC_APP_URL,
+  links: { github: 'https://github.com/tiavina-mika' },
+  author: AUTHOR_USERNAME,
+  senderEmail: 'tikskun@gmail.com',
+};
+
+export type SiteConfig = typeof siteConfig;
