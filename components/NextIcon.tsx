f1fa1ab210540416774254fec3c4d565b1747d3f
tiavina-mika <tiavinamika@gmail.com>
2024-03-13 18:51:32 +0300

    Create NextIcon.tsx

---
+++ components/NextIcon.tsx
@@ -0,0 +1,28 @@
+import Image, { ImageProps } from 'next/image';
+
+type Props = {
+  width?: number;
+  height?: number;
+  src: string;
+  className?: string;
+} & ImageProps;
+
+const NextIcon = ({
+  src,
+  className,
+  width = 24,
+  height = 24,
+  ...props
+}: Props) => (
+  <Image
+    {...props}
+    priority
+    src={src}
+    height={height}
+    width={width}
+    alt={props.alt || ''}
+    className={className}
+  />
+);
+
+export default NextIcon;
