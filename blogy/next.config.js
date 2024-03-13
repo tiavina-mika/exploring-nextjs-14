29019d937d50718b22c00ea8433498b28eb926f2
tiavina-mika <tiavinamika@gmail.com>
2024-03-13 18:39:35 +0300

    Delete next.config.js

--- blogy/next.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('next').NextConfig} */
-const withNextIntl = require('next-intl/plugin')('./config/i18n.ts');
-
-const nextConfig = {};
-
-module.exports = withNextIntl(nextConfig);
