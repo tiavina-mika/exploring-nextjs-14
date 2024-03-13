189c93b384059ae845579bc6ef0253cad3e9a86e
tiavina-mika <tiavinamika@gmail.com>
2024-03-13 10:26:26 +0300

    Update app.validations.ts

--- blogy/validations/app.validations.ts
+++ blogy/validations/app.validations.ts
@@ -1,7 +1,26 @@
+import { capitalize } from "string-ts";
 import { z } from "zod";
+import { zfd } from "zod-form-data";
+import { errorMap } from '@/config/zod';
+import { EmailFieldSchema } from "./auth.validations";
 
 export const paginationSearchParamsSchema = z.object({
   page: z.string().optional().default("1"),
   field: z.string().optional(),
   order: z.union([z.literal('asc'), z.literal('desc')]).optional().default("asc"),
 });
+
+export const ContactSchema = zfd.formData(
+  z.object({
+    email: EmailFieldSchema,
+    subject:  zfd.text(z.string({ errorMap })
+      .min(1,'error.required')
+      .max(112, 'error.max')
+      .transform(capitalize)),
+    message: zfd.text(z.string({ errorMap })
+      .min(10, 'error.max')
+      .max(500, 'error.max')
+      .optional()
+    ),
+  })
+);
