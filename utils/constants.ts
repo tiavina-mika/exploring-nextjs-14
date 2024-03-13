9fa760a200bdff7cdbb90922682be93348a194e8
tiavina-mika <tiavinamika@gmail.com>
2024-03-13 19:56:49 +0300

    Create constants.ts

---
+++ utils/constants.ts
@@ -0,0 +1,16 @@
+export const APP_NAME = 'Blogy';
+export const AUTHOR_USERNAME = 'tiavina-mika';
+
+export const collections = {
+  Article: 'Article',
+  Contact: 'Contact',
+};
+
+export const DISABLE_USER_ACCOUNT_CONFIRMATION = false;
+export const PAGINATION = {
+  perPage: 1,
+}
+
+export const COOKIES = {
+  locale: 'NEXT_LOCALE',
+}
