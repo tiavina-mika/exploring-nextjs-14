798c1145c8b6d53401b602b279b134028fa559e2
tiavina-mika <tiavinamika@gmail.com>
2024-03-13 18:49:21 +0300

    Delete user.utils.ts

--- blogy/utils/user.utils.ts
+++
@@ -1,6 +0,0 @@
-import { isCleanedString } from "./utils";
-
-export const capitalizeFirstLetter = (string: string): string => {
-  if (isCleanedString(string)) return '';
-  return string.charAt(0).toUpperCase() + string.slice(1);
-};
