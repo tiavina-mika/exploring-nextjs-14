542d8e6836b53006ea1e461f044df60d3e60af56
tiavina-mika <tiavinamika@gmail.com>
2024-03-13 13:01:41 +0300

    Update app.utils.ts

--- blogy/utils/app.utils.ts
+++ blogy/utils/app.utils.ts
@@ -1,12 +1,56 @@
+import { getCookie } from 'cookies-next';
 import env from '@/env';
-import { IListFilter, IPagination, IPaginationQuery } from '@/types/app.type';
+import { IListFilter, IPaginationQuery } from '@/types/app.type';
 import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
+import { COOKIES } from './constants';
+import { Locale, defaultLocale } from '@/config/i18n';
+import { ITranslatedPathnames, translatedRoutes } from '@/config/routes';
 
 export const cn = (...inputs: ClassValue[]) => twMerge(clsx(inputs));
 
-export const getAbsoluteUrl = (path?: string): string =>
-  `${env.NEXT_PUBLIC_APP_URL}${path}`;
+/**
+ * get current local from cookies, url or default config
+ * @param defaultLocal 
+ * @returns 
+ */
+const getCurrentLocale = (defaultLocal?: Locale | undefined | string) => {
+  const locale = defaultLocal || getCookie(COOKIES.locale) || defaultLocale;
+  return locale;
+}
+
+export const getAbsoluteUrl = (path?: string): string => {
+  return `${env.NEXT_PUBLIC_APP_URL}${path}`;
+};
+
+/**
+ * get the current url browser
+ * @param params ex: { locale: 'fr', articleId: '1' } 
+ * @param path 
+ * @returns 
+ */
+export const getTranslatedAbsoluteUrl = (path: string, params?: Record<string, string>): string => {
+  const locale = getCurrentLocale(params?.locale);
+  const routes = translatedRoutes[path] as ITranslatedPathnames;
+  const pathname = routes[locale] as string;
+
+  /**
+   * change the pathname params key to dynamic value
+   * ex: /dashboard/articles/[articleId] -> /dashboard/articles/1
+   */
+  if (params) {
+    let translatedPath = pathname;
+    Object.keys(params).forEach((key: string) => {
+      translatedPath = translatedPath.replace(`[${key}]`, params[key]);
+    });
+
+    const url = getAbsoluteUrl(`/${locale}${translatedPath}`);
+    return url;
+  }
+
+  const url = getAbsoluteUrl(`/${locale}${pathname}`);
+  return url;
+}
 
 export const getPaginatedQuery = ({ perPage, page, order, field }: IListFilter): IPaginationQuery => {
   return {
