d713f89dad38c912d46f8b1bec391a220464f220
tiavina-mika <tiavinamika@gmail.com>
2024-03-13 17:36:14 +0300

    Create page.tsx

---
+++ app/[locale]/page.tsx
@@ -0,0 +1,35 @@
+import env from '@/env';
+import { unstable_setRequestLocale } from 'next-intl/server';
+
+import LanguageSwitcher from '@/components/languages/LanguageSwitcher';
+import ToggleTheme from '@/components/ToggleTheme';
+import { Locale } from '@/config/i18n';
+
+type Props = {
+  params: {
+    locale: Locale;
+  };
+};
+
+const HomePage = ({ params: { locale } }: Props) => {
+  // const t = useTranslations('Common');
+
+  console.log('SERVER', env.SERVER);
+  console.log('NEXT_PUBLIC_CLIENT', env.NEXT_PUBLIC_CLIENT);
+  unstable_setRequestLocale(locale);
+
+
+  return (
+    <main className="flex min-h-screen flex-col p-6">
+      <div>
+        <ToggleTheme />
+      </div>
+      <div>
+        <LanguageSwitcher />
+      </div>
+      {/* <div>{t('greeting')}</div> */}
+    </main>
+  );
+};
+
+export default HomePage;
