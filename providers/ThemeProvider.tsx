d7e8982d48a36728bd34cc14eebdcd8abe2ff4c2
tiavina-mika <tiavinamika@gmail.com>
2024-03-13 18:55:03 +0300

    Create ThemeProvider.tsx

---
+++ providers/ThemeProvider.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import * as React from 'react';
+
+import { ThemeProvider as NextThemesProvider } from 'next-themes';
+import { type ThemeProviderProps } from 'next-themes/dist/types';
+
+const ThemeProvider = ({ children, ...props }: ThemeProviderProps) => (
+  <NextThemesProvider {...props}>{children}</NextThemesProvider>
+);
+
+export default ThemeProvider;
