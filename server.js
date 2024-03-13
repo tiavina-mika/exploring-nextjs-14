4879eb9f972b75fb6712f376464a2dfeb34daf48
tiavina-mika <tiavinamika@gmail.com>
2024-03-13 19:53:18 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,45 @@
+const next = require('next');
+const express = require('express');
+const cors = require('cors');
+const Parse = require('parse/node');
+
+const dev = process.env.NODE_ENV !== 'production';
+const hostname = 'localhost';
+const port = 9001;
+// when using middleware `hostname` and `port` must be provided below
+const nextApp = next({ dev, port });
+const handle = nextApp.getRequestHandler();
+
+nextApp
+  .prepare()
+  .then(() => {
+    const serverUrl = process.env.SERVER_URL;
+
+    Parse.serverURL = serverUrl + '/parse';
+    Parse.initialize(process.env.PARSE_APP_ID);
+    Parse.masterKey = 'local-master-key';
+
+    global.Parse = Parse;
+    const app = express();
+
+    app.use(cors());
+
+    app.all('*', (req, res) => {
+      return handle(req, res);
+    });
+
+    app.listen(port, () => {
+      console.log(
+        `
+        =====================================
+        NextJs server
+        http://${hostname}:${port}
+        =====================================
+        `,
+      );
+    });
+  })
+  .catch((error) => {
+    console.error(error.stack);
+    process.exit(1);
+  });
