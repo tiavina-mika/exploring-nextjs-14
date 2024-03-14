77f2675583f99a2ed54c12c1c21494f17b2f8feb
tiavina-mika <tiavinamika@gmail.com>
2024-03-14 12:48:00 +0300

    Update page.tsx

--- app/[locale]/dashboard/articles/[articleId]/edit/page.tsx
+++ app/[locale]/dashboard/articles/[articleId]/edit/page.tsx
@@ -7,6 +7,14 @@ import { notFound } from 'next/navigation';
 import { IArticle } from '@/types/article.type';
 import { Breadcrumbs } from '@/components/Breadcrumbs';
 import { ROUTES } from '@/config/routes';
+import { cache } from 'react';
+import { Metadata } from 'next';
+import { titleCase } from 'string-ts';
+
+const getCachedArticle = cache(async (articleId: string) => {
+  const article = await getArticle(articleId, true) as IArticle | undefined;
+  return article;
+})
 
 type Props = {
   params: {
@@ -15,11 +23,27 @@ type Props = {
   };
 };
 
+export const generateMetadata = async ({ params: { articleId, locale }}: Props): Promise<Metadata> => {
+  const t = await getTranslations({
+    locale,
+    namespace: 'Common',
+  });
+
+  const article: IArticle | undefined = await getCachedArticle(articleId);
+
+  if (!article) return {};
+
+  return {
+    title: t('edit') + ' ' + titleCase(article.title),
+    description: article.description || article.content,
+  };
+}
+
 const EditArticlePage = async ({ params: { locale, articleId } }: Props) => {
   unstable_setRequestLocale(locale);
   const t = await getTranslations('Common');
 
-  const article = await  getArticle(articleId, true);
+  const article = await  getCachedArticle(articleId);
 
   if (!article) {
     notFound();
@@ -35,8 +59,8 @@ const EditArticlePage = async ({ params: { locale, articleId } }: Props) => {
               href: (ROUTES.articles as any).root,
             },
             {
-              title: t('edit') + ' ' + (article as IArticle).title,
-              href: ROUTES.articles.preview((article as IArticle).objectId),
+              title: t('edit') + ' ' + article.title,
+              href: ROUTES.articles.preview(article.objectId),
             },
           ]}
         />
