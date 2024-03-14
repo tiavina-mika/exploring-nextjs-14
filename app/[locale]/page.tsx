c5b5a4c79ddf6da132a6e7ebc2afc1b28a426c61
tiavina-mika <tiavinamika@gmail.com>
2024-03-14 09:56:32 +0300

    Update page.tsx

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,9 +1,7 @@
-import env from '@/env';
 import { unstable_setRequestLocale } from 'next-intl/server';
 
-import LanguageSwitcher from '@/components/languages/LanguageSwitcher';
-import ToggleTheme from '@/components/ToggleTheme';
 import { Locale } from '@/config/i18n';
+import Title from '@/components/typography/Title';
 
 type Props = {
   params: {
@@ -12,23 +10,12 @@ type Props = {
 };
 
 const HomePage = ({ params: { locale } }: Props) => {
-  // const t = useTranslations('Common');
-
-  console.log('SERVER', env.SERVER);
-  console.log('NEXT_PUBLIC_CLIENT', env.NEXT_PUBLIC_CLIENT);
   unstable_setRequestLocale(locale);
 
-
   return (
-    <main className="flex min-h-screen flex-col p-6">
-      <div>
-        <ToggleTheme />
-      </div>
-      <div>
-        <LanguageSwitcher />
-      </div>
-      {/* <div>{t('greeting')}</div> */}
-    </main>
+    <div className="flex flex-col p-6">
+      <Title>Home</Title>
+    </div>
   );
 };
 
