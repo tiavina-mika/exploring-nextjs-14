e4cbfeccd760232cd77c557cb69697b1cb9201f7
tiavina-mika <tiavinamika@gmail.com>
2024-03-15 17:01:03 +0300

    Update page.tsx

--- app/[locale]/about/page.tsx
+++ app/[locale]/about/page.tsx
@@ -2,6 +2,7 @@ import { useTranslations } from 'next-intl';
 import { unstable_setRequestLocale } from 'next-intl/server';
 
 import { Locale } from '@/config/i18n';
+import Container from '@/components/Container';
 
 type Props = {
   params: {
@@ -13,7 +14,7 @@ const AboutPage = ({ params: { locale } }: Props) => {
   unstable_setRequestLocale(locale);
   const t = useTranslations('NavBar');
 
-  return <div>{t('about')} page</div>;
+  return <Container>{t('about')} page</Container>;
 };
 
 export default AboutPage;
