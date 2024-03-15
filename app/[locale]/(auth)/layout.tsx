a04a974c006d908b4e326d08a0ed1ca055592008
tiavina-mika <tiavinamika@gmail.com>
2024-03-15 17:01:00 +0300

    Update layout.tsx

--- app/[locale]/(auth)/layout.tsx
+++ app/[locale]/(auth)/layout.tsx
@@ -3,6 +3,7 @@ import { ReactNode } from 'react';
 import { unstable_setRequestLocale } from 'next-intl/server';
 
 import { Locale } from '@/config/i18n';
+import Container from '@/components/Container';
 
 type Props = {
   children: ReactNode;
@@ -14,11 +15,11 @@ const AuthLayout = async ({ children, params: { locale } }: Props) => {
   unstable_setRequestLocale(locale);
 
   return (
-    <main className="flex justify-center md:h-screen pt-0 md:pt-32">
+    <Container className="flex justify-center md:h-screen pt-0 md:pt-32">
       <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
         {children}
       </div>
-    </main>
+    </Container>
   );
 };
 
