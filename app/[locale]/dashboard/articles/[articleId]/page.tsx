c839479b5fc40e9f6f9a5c94fd19c48f7a8baddf
tiavina-mika <tiavinamika@gmail.com>
2024-03-18 17:25:52 +0300

    Update page.tsx

--- app/[locale]/dashboard/articles/[articleId]/page.tsx
+++ app/[locale]/dashboard/articles/[articleId]/page.tsx
@@ -13,6 +13,8 @@ import { cache } from 'react';
 import Container from '@/components/Container';
 import { getArticleCategoriesOptions } from '@/utils/article.utils';
 import { getSelectOptionByValue } from '@/utils/app.utils';
+import ArticleMoreMenus from '@/containers/articles/ArticleMoreMenus';
+import TranslationClientProvider from '@/components/TranslationClientProvider';
 
 const getCachedArticle = cache(async (articleId: string) => {
   const article = await getArticle(articleId) as Parse.Object | undefined;
@@ -45,6 +47,7 @@ export const generateMetadata = async ({ params: { articleId }}: Props): Promise
 // ----------------------------- //
 const ArticlePage = async ({ params: { locale, articleId } }: Props) => {
   unstable_setRequestLocale(locale);
+
   const t = await getTranslations('Article');
 
   const article = await getCachedArticle(articleId);
@@ -54,22 +57,29 @@ const ArticlePage = async ({ params: { locale, articleId } }: Props) => {
   }
 
   const categoriesOptions = getArticleCategoriesOptions(t);
-  const categories = article.get('categories')?.map((category: string) => getSelectOptionByValue(categoriesOptions, category, 'label'))
+  const categories = article.get('categories')?.map((category: string) => getSelectOptionByValue(categoriesOptions, category, 'label'));
 
   return (
     <Container className="flex flex-col">
-      <Breadcrumbs
-        segments={[
-          {
-            title: 'Articles',
-            href: (ROUTES.articles as any).root,
-          },
-          {
-            title: article.get('title'),
-            href: ROUTES.articles.preview(article.id),
-          },
-        ]}
-      />
+      <div className="self-stretch flex flex-row justify-between">
+        <Breadcrumbs
+          segments={[
+            {
+              title: 'Articles',
+              href: (ROUTES.articles as any).root,
+            },
+            {
+              title: article.get('title'),
+              href: ROUTES.articles.preview(article.id),
+            },
+          ]}
+        />
+        {/* pass the translated key from the server to a client component */}
+        <TranslationClientProvider rootKeys={['Common', 'Article']}>
+          <ArticleMoreMenus articleId={article.id} currentAction="preview" />
+        </TranslationClientProvider>
+      </div>
+
       <Card className="md:w-full self-stretch" contentClassName="space-y-3">
         <div className="flex flex-row items-center gap-6">
           <Text as="span" className="font-bold">{t('title')}</Text>
