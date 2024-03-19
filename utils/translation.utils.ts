99130f72167916391e632391d94c2b6f710776cf
tiavina-mika <tiavinamika@gmail.com>
2024-03-19 19:36:34 +0300

    Update translation.utils.ts

--- utils/translation.utils.ts
+++ utils/translation.utils.ts
@@ -1,3 +1,4 @@
+import { Locale, defaultLocale } from '@/config/i18n';
 import enApp from '@/translations/en/app.json';
 import enArticle from '@/translations/en/article.json';
 import enAuth from '@/translations/en/auth.json';
@@ -7,6 +8,18 @@ import frApp from '@/translations/fr/app.json';
 import frArticle from '@/translations/fr/article.json';
 import frAuth from '@/translations/fr/auth.json';
 import frCommon from '@/translations/fr/common.json';
+import { getCookie } from 'cookies-next';
+import { COOKIES } from './constants';
+
+/**
+ * get current local from cookies, url or default config
+ * @param defaultLocal
+ * @returns
+ */
+export const getCurrentLocale = (defaultLocal?: Locale | undefined | string) => {
+  const locale = defaultLocal || getCookie(COOKIES.locale) || defaultLocale;
+  return locale;
+}
 
 /**
  * load all the translations from all messages files for the given locale
