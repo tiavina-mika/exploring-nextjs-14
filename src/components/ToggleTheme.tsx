42ac3881c671a07b8be5638022b80b705fdf0035
tiavina-mika <tiavinamika@gmail.com>
2024-03-19 14:39:00 +0300

    Create ToggleTheme.tsx

---
+++ src/components/ToggleTheme.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import * as React from 'react';
+
+import { useTheme } from 'next-themes';
+
+import IconButton from './buttons/IconButton';
+import NextIcon from './NextIcon';
+
+const ToggleTheme = () => {
+  const { theme, setTheme } = useTheme();
+
+  return (
+    <IconButton
+      onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+      className="relative p-2 rounded-md !border-gray-100 border"
+    >
+      <NextIcon
+        alt=""
+        src="/icons/sun.svg"
+        width={19}
+        height={19}
+        className="rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+        aria-hidden="true"
+      />
+      <NextIcon
+        alt=""
+        src="/icons/moon.svg"
+        width={24}
+        height={24}
+        className="absolute top-[5px] left-[5px] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+        aria-hidden="true"
+      />
+
+      <span className="sr-only">Toggle theme</span>
+    </IconButton>
+  );
+};
+
+export default ToggleTheme;
