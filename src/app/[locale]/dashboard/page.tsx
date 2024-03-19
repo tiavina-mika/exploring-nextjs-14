3edc8a3d12fbb7449701015c2a35d1179bc20d94
tiavina-mika <tiavinamika@gmail.com>
2024-03-19 13:43:59 +0300

    Create page.tsx

---
+++ src/app/[locale]/dashboard/page.tsx
@@ -0,0 +1,45 @@
+import { getTranslations, unstable_setRequestLocale } from 'next-intl/server';
+
+import Title from '@/components/typography/Title';
+import { Locale } from '@/config/i18n';
+import { Metadata } from 'next';
+
+// ----------------------------- //
+// -------- metadata ----------- //
+// ----------------------------- //
+type MetaDataProps = {
+  params: { locale: Locale }
+}
+
+export const generateMetadata = async ({ params: { locale }}: MetaDataProps): Promise<Metadata> => {
+  const t = await getTranslations({
+    locale,
+    namespace: 'Metadata',
+  });
+
+  return {
+    title: t('dashboard.metaTitle'),
+    description: t('dashboard.metaDescription'),
+  };
+}
+
+// ----------------------------- //
+// ------------ page ----------- //
+// ----------------------------- //
+type Props = {
+  params: {
+    locale: Locale;
+  };
+};
+
+const DashboardHomePage = async ({ params: { locale } }: Props) => {
+  unstable_setRequestLocale(locale);
+
+  return (
+    <div>
+      <Title>Dashboard home page</Title>
+    </div>
+  );
+};
+
+export default DashboardHomePage;
