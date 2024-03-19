c822715c42eaa7e99d0923ad4b24aefec46cd0ea
tiavina-mika <tiavinamika@gmail.com>
2024-03-19 10:05:41 +0300

    Update Container.tsx

--- components/Container.tsx
+++ components/Container.tsx
@@ -11,7 +11,7 @@ type Props = {
 
 const Container: any = ({ children, className, as: Component = 'div', maxWidth = 'xl', withSpacingY = true }: Props) => {
   return (
-      <Component className={cn('w-full', className, {
+      <Component className={cn('w-full px-6', className, {
         'max-w-screen-xl': maxWidth === 'xl',
         'max-w-screen-md': maxWidth === 'md',
         'max-w-screen-lg': maxWidth === 'lg',
