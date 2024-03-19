de496067f3d2a603f5a9430209c2ab6db6d6fb12
tiavina-mika <tiavinamika@gmail.com>
2024-03-19 19:11:09 +0300

    Update global.d.ts

--- global.d.ts
+++ global.d.ts
@@ -1,3 +1,8 @@
 // Use type safe message keys with `next-intl`
-type Messages = typeof import('./translations/en.json');
+type Messages =
+  typeof import('./translations/en/app.json')
+  & typeof import('./translations/en/article.json')
+  & typeof import('./translations/en/auth.json')
+  & typeof import('./translations/en/common.json');
+  
 declare interface IntlMessages extends Messages {}
