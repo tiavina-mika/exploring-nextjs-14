ab719c79a05f0458e951f0b15f42b239dbca610a
tiavina-mika <tiavinamika@gmail.com>
2024-03-19 08:56:45 +0300

    Update page.tsx

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,13 +1,12 @@
 import { unstable_setRequestLocale } from 'next-intl/server';
 
 import { Locale } from '@/config/i18n';
-import Title from '@/components/typography/Title';
-import Text from '@/components/typography/Text';
-import Button from '@/components/buttons/Button';
-import Container from '@/components/Container';
-import NextIcon from '@/components/NextIcon';
+import HomeHeader from '@/containers/home/HomeHeader';
+import { IInfo } from '@/types/app.type';
+import HomeAdvantages from '@/containers/home/HomeAdvantages';
+import HomeFeatures from '@/containers/home/HomeFeatures';
 
-const sections = [
+const advantages: IInfo[] = [
   {
     title: "Voluptas doloribus asperiores ipsum",
     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas doloribus asperiores quaerat aperiam. Quidem harum omnis beatae ipsum"
@@ -22,7 +21,7 @@ const sections = [
   }
 ];
 
-const advantages = [
+const features = [
   {
     icon: "/icons/eye-off.svg",
     title: "Voluptas doloribus asperiores",
@@ -51,75 +50,9 @@ const HomePage = ({ params: { locale } }: Props) => {
 
   return (
     <div className="self-stretch flex flex-col md:items-center">
-      {/* headers */}
-      <header className="self-stretch flex flex-col items-center justify-center gap-8 min-h-[500px] bg-gray-200">
-        <div className="max-w-md">
-          <div className="self-stretch flex flex-col md:items-center gap-8">
-            <Title level="h1" className="!text-6xl font-extrabold">
-              Some title here
-            </Title>
-            <Text className="text-center">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas doloribus asperiores quaerat aperiam. Quidem harum omnis beatae ipsum soluta!
-            </Text>
-          </div>
-          <div className="flex flex-row items-center gap-5 mt-9">
-            <Button className="rounded-3xl py-3">
-              Download the app
-            </Button>
-            <Button variant='outlined' className="rounded-3xl py-3">
-              Download the app
-            </Button>
-          </div>
-        </div>
-      </header>
-
-      <Container as="section" className="flex items-center gap-9 min-h-[500px] md:px-20" withSpacingY={false}>
-        {sections.map((section) => (
-          <div key={section.title} className="flex flex-col gap-4 items-center">
-            <Title level="h2" className="md:text-center md:text-3xl font-bold !leading-tight">{section.title}</Title>
-            <Text className="md:text-center md:text-md">{section.description}</Text>
-          </div>
-        ))}
-      </Container>
-
-      <div className="flex md:justify-center bg-gray-200 self-stretch py-36">
-        <Container className="flex items-center gap-9" maxWidth="lg" withSpacingY={false}>
-          {/* left */}
-          <div className="flex-1 stretchSelf flex flex-col md:items-center gap-6">
-            <Title level="h2" className="md:text-5xl font-bold !leading-snug">
-              A super interesting title here
-            </Title>
-            <Text className="text-xl">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas doloribus asperiores quaerat aperiam. Quidem harum omnis beatae ipsum soluta!
-            </Text>
-            <div className="flex flex-col gap-6">
-              {advantages.map(((advantage, index) => (
-                <div key={advantage.title + index} className="flex self-stretch">
-                  <div className="pr-6 py-2">
-                    <NextIcon
-                      alt={advantage.title}
-                      width={32}
-                      height={32}
-                      src={advantage.icon}
-                    />
-                  </div>
-                  <div className="flex flex-col gap-1">
-                    <Title level="h3" className="font-bold">
-                      {advantage.title}
-                    </Title>
-                    <Text>
-                      {advantage.description}
-                    </Text>
-                  </div>
-                </div>
-              )))}
-            </div>
-          </div>
-          <div className="flex-1">
-            Image here
-          </div>
-        </Container>
-      </div>
+      <HomeHeader />
+      <HomeAdvantages sections={advantages} />
+      <HomeFeatures sections={features} />
     </div>
   );
 };
