f84788df99fc01b6500fc6191e53d7dea6d8f692
tiavina-mika <tiavinamika@gmail.com>
2024-03-20 08:54:09 +0300

    Update global.d.ts

--- global.d.ts
+++ global.d.ts
@@ -3,6 +3,7 @@ type Messages =
   typeof import('./translations/en/app.json')
   & typeof import('./translations/en/article.json')
   & typeof import('./translations/en/auth.json')
-  & typeof import('./translations/en/common.json');
-  
+  & typeof import('./translations/en/common.json')
+  & typeof import('./translations/en/zod.json');
+
 declare interface IntlMessages extends Messages {}
