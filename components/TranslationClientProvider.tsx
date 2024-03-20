d9b7778ba298a42aaa1fe04ea1930bf029ca2247
tiavina-mika <tiavinamika@gmail.com>
2024-03-20 09:29:40 +0300

    Update TranslationClientProvider.tsx

--- components/TranslationClientProvider.tsx
+++ components/TranslationClientProvider.tsx
@@ -1,26 +1,35 @@
-'use client';
-
 import { ReactNode } from 'react';
 
 import {
   NextIntlClientProvider,
   useLocale,
+  useMessages,
 } from 'next-intl';
 
 import { Locale } from '@/config/i18n';
-import { getTranslatedMessages } from '@/utils/translation.utils';
+import { pick } from 'lodash';
 
+/**
+ * get only a selected translated messages by its keys
+ * so thant we doesn't have to load all the translations in the client
+ * ex: get only Common, Form, ...
+ * @param locale
+ * @param rootKeys
+ * @returns
+ */
 type Props = {
   children: ReactNode;
   rootKeys: string[];
 };
+
 const TranslationClientProvider: any = ({ children, rootKeys = [] }: Props) => {
   const locale = useLocale() as Locale;
+  const messages = useMessages();
 
   return (
     <NextIntlClientProvider
       locale={locale}
-      messages={getTranslatedMessages(locale)}
+      messages={pick(messages, rootKeys)}
       timeZone="UTC"
     >
       {children}
