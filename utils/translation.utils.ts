05889a074990393efea1a4f14fdc44188800f693
tiavina-mika <tiavinamika@gmail.com>
2024-03-20 08:54:23 +0300

    Update translation.utils.ts

--- utils/translation.utils.ts
+++ utils/translation.utils.ts
@@ -3,11 +3,14 @@ import enApp from '@/translations/en/app.json';
 import enArticle from '@/translations/en/article.json';
 import enAuth from '@/translations/en/auth.json';
 import enCommon from '@/translations/en/common.json';
+import enZod from '@/translations/en/zod.json';
 
 import frApp from '@/translations/fr/app.json';
 import frArticle from '@/translations/fr/article.json';
 import frAuth from '@/translations/fr/auth.json';
 import frCommon from '@/translations/fr/common.json';
+import frZod from '@/translations/fr/zod.json';
+
 import { getCookie } from 'cookies-next';
 import { COOKIES } from './constants';
 
@@ -34,6 +37,7 @@ export const getTranslatedMessages = (locale: string) => {
         ...enArticle,
         ...enAuth,
         ...enCommon,
+        ...enZod,
       };
 
     // french is the default
@@ -43,6 +47,7 @@ export const getTranslatedMessages = (locale: string) => {
         ...frArticle,
         ...frAuth,
         ...frCommon,
+        ...frZod,
       };
   }
 }
