f8a1d69a8b880d7e7b27faf866323c0e31d4dd17
tiavina-mika <tiavinamika@gmail.com>
2024-03-24 12:17:14 +0300

    Update AuthLink.tsx

--- containers/auth/AuthLink.tsx
+++ containers/auth/AuthLink.tsx
@@ -9,7 +9,7 @@ type Props = {
 
 const AuthLink = ({ text, url, label }: Props) => {
   return (
-    <Text as="p" className="flex">{label} <TextLink href={url} className="pl-2">{text}</TextLink></Text>
+    <Text as="p" className="md:flex text-sm md:text-md">{label} <TextLink href={url} className="md:pl-2 text-sm md:text-md">{text}</TextLink></Text>
   );
 };
 
