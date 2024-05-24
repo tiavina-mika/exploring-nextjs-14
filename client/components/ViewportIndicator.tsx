5436daa270469da5bfba0b475be3ee791396814e
tiavina-mika <tiavinamika@gmail.com>
2024-05-24 16:55:15 +0300

    Create ViewportIndicator.tsx

---
+++ client/components/ViewportIndicator.tsx
@@ -0,0 +1,20 @@
+import env from '@/env';
+
+const ViewportIndicator = () => {
+  if (env.NODE_ENV === 'production') return null;
+
+  return (
+    <div className="size-6 fixed bottom-1 left-1 z-50 flex items-center justify-center rounded-full bg-gray-800 p-3 font-mono text-xs text-white">
+      <div className="block sm:hidden">xs</div>
+      <div className="hidden sm:block md:hidden lg:hidden xl:hidden 2xl:hidden">
+        sm
+      </div>
+      <div className="hidden md:block lg:hidden xl:hidden 2xl:hidden">md</div>
+      <div className="hidden lg:block xl:hidden 2xl:hidden">lg</div>
+      <div className="hidden xl:block 2xl:hidden">xl</div>
+      <div className="hidden 2xl:block">2xl</div>
+    </div>
+  );
+};
+
+export default ViewportIndicator;
