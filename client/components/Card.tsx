630fb6207b6fe48b04fc6bf04ac190cb9c8ccc2c
tiavina-mika <tiavinamika@gmail.com>
2024-06-07 13:30:33 +0300

    Create Card.tsx

---
+++ client/components/Card.tsx
@@ -0,0 +1,83 @@
+import { ReactNode } from 'react';
+
+import {
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+  Card as UICard,
+} from '@/components/ui/Card';
+import { cn } from '@/utils/app.utils';
+
+import Button from './buttons/Button';
+
+type Props = {
+  children: ReactNode;
+  className?: string;
+  contentClassName?: string;
+  title?: string;
+  description?: string;
+  primaryButtonText?: string;
+  secondaryButtonText?: string;
+  onPrimaryButtonClick?: () => void;
+  onSecondaryButtonClick?: () => void;
+  buttonsDirection?: 'row' | 'column';
+};
+const Card = ({
+  children,
+  className,
+  description,
+  title,
+  primaryButtonText,
+  secondaryButtonText,
+  buttonsDirection,
+  contentClassName,
+  onPrimaryButtonClick,
+  onSecondaryButtonClick,
+}: Props) => (
+  <UICard className={cn('w-full md:w-[350px]', className)}>
+    {/* title and description */}
+    {title ||
+      (description && (
+        <CardHeader>
+          {title && <CardTitle>{title}</CardTitle>}
+          {description && <CardDescription>{description}</CardDescription>}
+        </CardHeader>
+      ))}
+    <CardContent className={contentClassName}>
+      {children}
+    </CardContent>
+    {/* footer */}
+    {onPrimaryButtonClick ||
+      (onSecondaryButtonClick && (
+        <CardFooter
+          className={
+            buttonsDirection === 'row'
+              ? 'flexRow center spaceBetween'
+              : 'flexColumn gap-3'
+          }
+        >
+          {/* cancel button */}
+          {secondaryButtonText && (
+            <Button
+              onClick={onSecondaryButtonClick}
+              className={buttonsDirection === 'column' ? 'stretchSelf' : ''}
+            >
+              {secondaryButtonText}
+            </Button>
+          )}
+          {/* submit button */}
+          <Button
+            variant="contained"
+            onClick={onPrimaryButtonClick}
+            className={buttonsDirection === 'column' ? 'stretchSelf' : ''}
+          >
+            {primaryButtonText}
+          </Button>
+        </CardFooter>
+      ))}
+  </UICard>
+);
+
+export default Card;
